0448ce91b32c96a45cd6b2792e73f41783dbca64
mukibkhan <112566644+mukibkhan@users.noreply.github.com>
2022-09-01 22:56:18 +0530

    Create DatabaseCreator.sql

---
+++ DatabaseCreator.sql
@@ -0,0 +1,8 @@
+#CALL generate_room(1, 'Ground', 100);
+#CALL generate_room(1, 'First', 100);
+#CALL generate_room(1, 'Second', 50);
+#CALL generate_room(2, 'Ground', 100);
+#CALL generate_room(2, 'First', 100);
+#CALL generate_room(2, 'Second', 50);
+
+select * from room;
